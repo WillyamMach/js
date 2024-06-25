0ed649532558cdedd598b67dd4c3af51e036fc41
Willyam Mach <willyam.mach@gmail.com>
2024-06-25 20:48:15 -0300

    array and object

---
+++ stage04/arrays.js
@@ -0,0 +1,20 @@
+const animals = 
+    [{
+        name: 'dog',
+        weight: '3kg',
+    },
+    {
+        name: "cat",
+        weight: '10kg'
+    },
+    {
+        name: "rat",
+        weight: '0.5kg'
+    }
+    ];
+
+console.log(animals[2].name, animals[2].weight)
+
+
+
+
