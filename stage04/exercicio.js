f90b4708661ff1e55bc183dc9cbd9141b70c325b
Willyam Mach <willyam.mach@gmail.com>
2024-06-25 21:06:10 -0300

    exercicio 1

---
+++ stage04/exercicio.js
@@ -0,0 +1,44 @@
+// 1
+let weight = '10kg';
+
+// 2
+console.log(typeof(weight));
+
+// 3
+const person = {
+    name: 'Willyam',
+    age: 19,
+    weight: 89,
+    isSubscribed: true
+}
+
+// 4
+console.log(`${person.name} de idade ${person.age} e ${person.weight} kg`)
+
+// 5
+let students = [];
+
+// 6 
+students = [person]
+
+// 7
+console.log(students[0]);
+
+//  8
+const student = {
+    name: "jonas",
+    age: 18,
+    weight: 70,
+    isSubscribed: false
+}
+
+students[1] = student;
+
+// 9
+console.log(a)
+var a = 1;
+
+
+
+
+
