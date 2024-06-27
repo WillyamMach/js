bff3e1401485c688015a5efcfeb0764d30fbfd24
Willyam Mach <willyam.mach@gmail.com>
2024-06-27 17:10:55 -0300

    aula f114

---
+++ stage04/funcoes/script.js
@@ -0,0 +1,7 @@
+createPhrase();
+
+function createPhrase() {
+    console.log("Estudar é muito bom");
+    console.log("paciencia e persintencia");
+    console.log("Revisao é a mae do aprendizado");
+}
