517acf1488bb80e7e68bcf0bb8f3ac7e20e6cd9c
Willyam Mach <willyam.mach@gmail.com>
2024-06-27 17:44:40 -0300

    functions

--- stage04/funcoes/script.js
+++ stage04/funcoes/script.js
@@ -1,7 +1,44 @@
-createPhrase();
+// createPhrase();
 
-function createPhrase() {
-    console.log("Estudar é muito bom");
-    console.log("paciencia e persintencia");
-    console.log("Revisao é a mae do aprendizado");
+// function createPhrase() {
+//     console.log("Estudar é muito bom");
+//     console.log("paciencia e persintencia");
+//     console.log("Revisao é a mae do aprendizado");
+// }
+
+// const sum = function(number1, number2) {
+//     return (number1 + number2);
+// }
+
+// const multiplication = (number1, number2) => {
+//     return (number1 * number2);
+// }
+
+// let number1 = 10;
+// let number2 = 3;
+
+// console.log(`result = ${multiplication(number1, number2)}`)
+
+// let object;
+
+// function returnObject(object) {
+//     return object = "123";
+//     // return object
+// }
+
+// console.log(object)
+// console.log(returnSubject(object))
+// console.log(object)
+
+// callback function
+
+function myName(name) {
+    name();
 }
+
+myName(
+    () => {
+        console.log('name');
+    }
+) 
+
