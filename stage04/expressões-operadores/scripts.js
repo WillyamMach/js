932fbc5c5d6423d500f6743c13ce4d9ae8b2fb88
Willyam Mach <willyam.mach@gmail.com>
2024-07-03 18:53:33 -0300

    operadores e expressões

---
+++ stage04/expressões-operadores/scripts.js
@@ -0,0 +1,10 @@
+// ternario
+console.log(true ? "oi" : 'hello')
+
+// left hand side <- instancia objetos
+let name = new String('Willyam');
+let age = new Number(23);
+
+name.surName = "Roger";
+
+console.log(typeof name.surName)
